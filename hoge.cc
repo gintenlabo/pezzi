2a09ba5dab079ec53dae13c983de8c5c13f882ed
SubaruG <gintensubaru@gmail.com>
2011-06-05 15:32:58 +0900

    hoge.cc を別の例に変更

--- hoge.cc
+++ hoge.cc
@@ -2,34 +2,22 @@
 // C++ インタプリタ欲しいなー。無理だろうけど。
 // このコメントはエンコードを UTF-8 にするために書いてるのです。
 
-#include <random>
-#include <ctime>
+#include <cstdint>
+#include <boost/config.hpp>
+
+inline BOOST_CONSTEXPR std::uint32_t rotate_left_32( std::uint32_t x, std::size_t n ) {
+  return ( x << ( n % 32 ) ) | ( x >> ( 32 - ( n % 32 ) ) );
+}
+
 #include <iostream>
-#include <algorithm>
-#include <functional>
 
 int main()
 {
-  std::uint32_t seed = static_cast<std::uint32_t>( std::time(0) );
-  
-  std::mt19937 gen( seed );
-  
-  std::size_t const teisu    = 10000;
-  std::size_t const kohosha  = 20000;
-  std::size_t const yukensha = teisu * teisu;
-  
-  std::uniform_int_distribution<> dist( 0, kohosha - 1 );
-  
-  std::vector<int> koho_list( kohosha, 0 );
-  for( std::size_t i = 0; i < yukensha; ++i ) {
-    std::size_t const vote = dist( gen );
-    koho_list[vote] += 1;
-  }
-  
-  std::sort( koho_list.begin(), koho_list.end(), std::greater<int>() );
-  
-  std::cout << "最大得票： " << koho_list[0] << std::endl;
-  std::cout << "当選ライン： " << koho_list[teisu-1] << std::endl;
-  std::cout << "落選ライン： " << koho_list[teisu] << std::endl;
-  std::cout << "最低得票： " << koho_list[kohosha-1] << std::endl;
+  std::cout << std::hex;
+  std::cout << rotate_left_32( 0x11,  4 ) << std::endl;
+  std::cout << rotate_left_32( 0x11, 36 ) << std::endl;
+  std::cout << rotate_left_32( 0x11, -4 ) << std::endl;
+  std::cout << rotate_left_32( 0x11, 28 ) << std::endl;
+  std::cout << rotate_left_32( 0x11,  0 ) << std::endl;
+  std::cout << rotate_left_32( 0x11, 32 ) << std::endl;
 }
