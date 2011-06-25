8a40ad9db63f6c6de83ea9e33849179cc8f71c4e
SubaruG <gintensubaru@gmail.com>
2011-06-25 17:46:36 +0900

    shared_ptr<function<...>> のパターンを考えた

--- hoge.cc
+++ hoge.cc
@@ -2,13 +2,57 @@
 // C++ インタプリタ欲しいなー。無理だろうけど。
 // このコメントはエンコードを UTF-8 にするために書いてるのです。
 
-#include <atomic>
-#include <type_traits>
+#include <memory>
+#include <functional>
 
 #define STATIC_ASSERT(...) static_assert( __VA_ARGS__, #__VA_ARGS__ )
 
+#include <boost/noncopyable.hpp>
+#include <iostream>
+
+struct X
+  : private boost::noncopyable
+{
+  explicit X( int m_ )
+    : member( m_ ) {}
+  
+  ~X() {
+    std::cout << "X::~X()\n";
+  }
+  
+  void foo() {
+    std::cout << "The member is " << member << ".\n";
+  }
+  
+ private:
+  int member;
+  
+};
+
+std::shared_ptr<std::function<void()>> make_some_function()
+{
+  struct impl_
+    : X
+  {
+    std::function<void()> f;
+    
+    impl_( int m_ )
+      : X(m_) {}
+    
+  };
+  
+  auto  p = std::make_shared<impl_>( 42 );
+  auto& f = p->f;
+  auto& x = static_cast<X&>(*p);
+  
+  f = [&x]{ x.foo(); };
+  
+  return std::shared_ptr<std::function<void()>>( p, &f );
+}
+
+
 int main()
 {
-  STATIC_ASSERT( std::is_class<std::atomic_int>::value );
-  STATIC_ASSERT( std::is_base_of<std::atomic_int, std::atomic<int>>::value );
+  auto pf = make_some_function();
+  (*pf)();
 }
