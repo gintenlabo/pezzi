bd43c52351741bdfc02dbb1b2255a744f6d6a195
Takaya SAITO (SubaruG) <gintensubaru@gmail.com>
2011-07-17 00:20:00 +0900

    Insertion Sort を旧バージョンに戻す

--- sort/insertion_sort.hpp
+++ sort/insertion_sort.hpp
@@ -6,31 +6,36 @@
 
 namespace pezzi
 {
-  // 挿入ソート
+  // pos を [ first, last ) の中の適切な位置に挿入
+  // [ first, last ) はソート済みでなければならない
+  // pos の位置には最大の要素が入る
+  // なお last == pos でも構わない
+  template< class Iter, class Comp >
+  void insert_sorted( Iter first, Iter last, Iter pos, Comp comp )
+  {
+    if( first == last ){ return; }
+    --last;
+    
+    while( comp( *pos, *last ) ) {
+      std::iter_swap( last, pos );
+      
+      if( first == last ) {
+        return;
+      }
+      pos = last; --last;
+    }
+  }
+  
+  // 挿入ソート本体
   template< class Iter, class Comp >
   void insertion_sort( Iter first, Iter last, Comp comp )
   {
-    typedef typename std::iterator_traits<Iter>::value_type value_type;
     if( first == last ){ return; }
     
     for( Iter i = first, j = std::next(i); j != last; ++i, ++j ) {
       if( comp( *j, *i ) ) {
-        value_type temp = std::move(*j);
-        Iter k = i;
-        *j = std::move(*k);
-        for(;;)
-        {
-          if( k == first ){
-            *k = std::move(temp);
-            break;
-          }
-          Iter l = k; --k;
-          if( !comp( temp, *k ) ) {
-            *l = std::move(temp);
-            break;
-          }
-          *l = std::move(*k);
-        }
+        std::iter_swap( i, j );
+        insert_sorted( first, i, i, comp );
       }
     }
   }
