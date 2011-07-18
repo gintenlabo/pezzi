3d4be016d83f5ddf4146706dda96feef1c3888be
Takaya SAITO (SubaruG) <gintensubaru@gmail.com>
2011-07-18 20:44:14 +0900

    suffix array のテストを変更

--- suffix_array/c_suffix_array.cc
+++ suffix_array/c_suffix_array.cc
@@ -3,19 +3,29 @@
 #include <iostream>
 #include <boost/foreach.hpp>
 
-int main()
+int main( int argc, char* argv[] )
 {
-  pezzi::c_suffix_array sa( "KOTOKO" );
-  auto const str = sa.c_str();
+  // 入力
+  char const* const target_string = ( argc > 1 ) ? argv[1] : "KOTOKO";
+  char const* const search_string = ( argc > 2 ) ? argv[2] : "KO";
   
+  // suffix array の構築
+  pezzi::c_suffix_array sa( target_string );
+  
+  // ダンプ
   BOOST_FOREACH( auto const i, sa ) {
-    std::cout << i << ", " << ( str + i ) << std::endl;
+    std::cout << i << ", " << ( target_string + i ) << std::endl;
   }
+  std::cout << std::endl;
   
-  auto const found_ = sa.search( "KO" );
+  // 検索
+  auto const found_ = sa.search( search_string );
+  
+  // 検索結果を vector に変形，ソート
   std::vector<int> found( found_.first, found_.second );
   std::sort( found.begin(), found.end() );
   
+  // 結果の表示
   BOOST_FOREACH( auto const i, found ) {
     std::cout << i << std::endl;
   }
