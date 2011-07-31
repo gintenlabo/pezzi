826b499ba695f32b191abe011245ab5e231f73fb
Takaya SAITO (SubaruG) <gintensubaru@gmail.com>
2011-07-31 19:11:24 +0900

    hoge

--- hoge.cc
+++ hoge.cc
@@ -3,49 +3,14 @@
 // このコメントはエンコードを UTF-8 にするために書いてるのです。
 
 #include <iostream>
-
-struct X
-{
-  explicit X( int m_ )
-    : member( m_ ) {}
-  
-  ~X() {
-    std::cout << "X::~X()\n";
-  }
-  
-  void foo() {
-    std::cout << "The member is " << member << ".\n";
-  }
-  
- private:
-  int member;
-  
-};
-
-#include <memory>
-#include <functional>
-#include <utility>
-#include <tuple>
-
-std::shared_ptr<std::function<void()>> make_some_function()
-{
-  typedef std::function<void()> function_t;
-  typedef std::pair<X, function_t> pair_t;
-  
-  auto  p = std::make_shared<pair_t>( std::piecewise_construct,
-    std::make_tuple( 42 ), std::make_tuple()
-  );
-  auto& x = p->first;
-  auto& f = p->second;
-  
-  f = [&x]{ x.foo(); };
-  
-  return std::shared_ptr<std::function<void()>>( p, &f );
-}
-
+#include "get_typename.hpp"
 
 int main()
 {
-  auto pf = make_some_function();
-  (*pf)();
+  struct X {
+    int i;
+  };
+  
+  X const x = {0};
+  std::cout << pezzi::get_typename<decltype(x.i)>() << std::endl;
 }
