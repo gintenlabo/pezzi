68f3d425d0c83bcf2e53ae54eed2e0556f9425fe
Takaya SAITO (SubaruG) <gintensubaru@gmail.com>
2011-08-09 23:18:30 +0900

    バグ発見．

--- bug.cc
+++ bug.cc
@@ -1,12 +1,18 @@
-#include <iostream>
+#define STATIC_ASSERT( expr ) static_assert( expr, #expr )
 
-void f( int const &i )
-{
-  std::cout << [=](){ return i; }();
-}
+// tested function
+int f( int );
+
+STATIC_ASSERT( sizeof( f({}) ) == sizeof(int) );  // OK
+
+auto g()
+  -> decltype( f({}) );  // OK
+
+template<
+  class R = decltype( f({}) ) // fails to compile here
+>
+R h();
 
 int main()
 {
-  int x = 42;
-  f(x); // uninitialized value
 }
