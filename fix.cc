3e28be82731ad7f5e14033b738dce2e67ff90f2d
Takaya SAITO (SubaruG) <gintensubaru@gmail.com>
2011-09-05 19:36:51 +0900

    fix を読みにくかったので単純化

--- fix.cc
+++ fix.cc
@@ -1,10 +1,13 @@
+// 不動点コンビネータ in C++11
+// http://d.hatena.ne.jp/iorate/20110729/1311949434 に対し，
+// std::ref を使って高速化．
+
 #include <utility>
 #include <functional>
 
 namespace pezzi
 {
-  // ref 版
-  struct fix1_t
+  struct fix_t
   {
     template< class F >
     struct result
@@ -36,42 +39,7 @@ namespace pezzi
     }
     
   };
-  constexpr fix1_t fix1 = {};
-  
-  // オリジナル
-  struct fix2_t
-  {
-    template< class F >
-    struct result
-    {
-      typedef result<F> self_type;
-      
-      F f;
-      
-      template< class... Args,
-        class Result = decltype (
-          std::declval<F const&>()(
-            std::declval<self_type const&>(),
-            std::declval<Args>()...
-          )
-        )
-      >
-      Result operator()( Args&&... args ) const
-      {
-        return f( *this, std::forward<Args>(args)... );
-      }
-      
-    };
-    
-    template< class F >
-    constexpr auto operator()( F && f )
-      -> result<typename std::decay<F>::type>
-    {
-      return { std::forward<F>(f) };
-    }
-    
-  };
-  constexpr fix2_t fix2 = {};
+  constexpr fix_t fix = {};
   
 } // namespace pezzi
 
@@ -80,25 +48,15 @@ namespace pezzi
 
 int main()
 {
-  // boost::progress_timer t;
+  boost::progress_timer t;
   
   // 竹内関数でテスト
-  auto tarai_ = []( std::function<int (int, int, int)> f, int x, int y, int z ) { 
-    return ( x <= y ) ? y : f( f( x-1, y, z ), f( y-1, z, x ), f( z-1, x, y ) );
-  };
+  auto tarai = pezzi::fix(
+    []( std::function<int (int, int, int)> f, int x, int y, int z ) { 
+      return ( x <= y ) ? y : f( f( x-1, y, z ), f( y-1, z, x ), f( z-1, x, y ) );
+    }
+  );
   
-  auto tarai1 = pezzi::fix1( tarai_ );
-  auto tarai2 = pezzi::fix2( tarai_ );
-  
-  {
-    std::cout << "ref版：\n";
-    boost::progress_timer t;
-    std::cout << tarai1( 12, 6, 0 ) << '\n';
-  }
-  {
-    std::cout << "オリジナル：\n";
-    boost::progress_timer t;
-    std::cout << tarai2( 12, 6, 0 ) << '\n';
-  }
+  std::cout << tarai( 12, 6, 0 ) << '\n';
   
 }
