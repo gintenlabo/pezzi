4e024589b984828ef9deeae5d9e2675089b9cc8c
Takaya SAITO <saito.takaya@plus-zero.co.jp>
2024-09-13 20:47:29 +0900

    add eslint

---
+++ ts/eslint.config.mjs
@@ -0,0 +1,45 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import { includeIgnoreFile } from "@eslint/compat";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, ".gitignore");
+
+export default tseslint.config(
+  includeIgnoreFile(gitignorePath),
+  eslint.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "eqeqeq": ["error", "always", { "null": "ignore" }],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          "varsIgnorePattern": "^_",
+          "args": "all",
+          "argsIgnorePattern": "^_",
+          "caughtErrors": "all",
+          "caughtErrorsIgnorePattern": "^_",
+          "ignoreRestSiblings": true,
+        },
+      ],
+      "@typescript-eslint/restrict-template-expressions": [
+        "error",
+        {
+          "allowNumber": true,
+          "allowBoolean": true,
+          "allowNever": true,
+        },
+      ],
+    },
+  },
+);
