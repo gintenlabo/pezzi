24de2728ca470206d4ac6e0a00a897cfd85d4762
Takaya SAITO (SubaruG) <gintensubaru@gmail.com>
2024-09-14 23:15:27 +0900

    Add loop-only replicate benchmark

---
+++ ts/benchmark/replicate-loop-only.ts
@@ -0,0 +1,29 @@
+import * as Benchmark from 'benchmark';
+
+const replicateByFrom = <const T>(n: number, x: T): T[] => Array.from({ length: n }, () => x);
+
+const replicateByFill = <const T>(n: number, x: T): T[] => Array<T>(n).fill(x);
+
+const benchArray = (arr: readonly number[]) => {
+  void arr.reduce((x, y) => x + y, 0);
+};
+const arrByFrom = replicateByFrom(10000000, 5);
+const arrByFill = replicateByFill(10000000, 5);
+
+const suite = new Benchmark.Suite();
+
+suite
+  .add('Array.from version', () => {
+    benchArray(arrByFrom);
+  })
+  .add('fill version', () => {
+    benchArray(arrByFill);
+  })
+  .on('cycle', (event: Benchmark.Event) => {
+    console.log(String(event.target));
+  })
+  .on('complete', function (this: Benchmark.Suite) {
+    const fastests = this.filter('fastest').map('name') as string[];
+    console.log(`Fastest is ${JSON.stringify(fastests)}`);
+  })
+  .run({ async: true });
