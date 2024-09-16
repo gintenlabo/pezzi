e327d65323d0c754ec37a87fdf917525536d7cac
Takaya SAITO (SubaruG) <gintensubaru@gmail.com>
2024-09-16 16:13:09 +0900

    add generator version benchmark

--- ts/benchmark/replicate.ts
+++ ts/benchmark/replicate.ts
@@ -4,6 +4,13 @@ const replicateByFrom = <const T>(n: number, x: T): T[] => Array.from({ length: 
 
 const replicateByFill = <const T>(n: number, x: T): T[] => Array<T>(n).fill(x);
 
+function* replicateByGenerator_<const T>(n: number, x: T): Generator<T> {
+  for (let i = 0; i < n; i += 1) {
+    yield x;
+  }
+}
+const replicateByGenerator = <const T>(n: number, x: T): T[] => [...replicateByGenerator_<T>(n, x)];
+
 const benchReplicate = (replicate: (n: number, x: number) => number[]) => {
   const arr = replicate(10000000, 5);
   for (let i = 0; i < 5; i += 1) {
@@ -19,6 +26,9 @@ suite
   })
   .add('fill version', () => {
     benchReplicate(replicateByFill);
+  })
+  .add('generator version', () => {
+    benchReplicate(replicateByGenerator);
   })
   .on('cycle', (event: Benchmark.Event) => {
     console.log(String(event.target));
