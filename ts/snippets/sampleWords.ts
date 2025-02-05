edb4fc226a8c72e921b5269c326a226f0b812bf6
Takaya SAITO (SubaruG) <gintensubaru@gmail.com>
2025-02-06 04:45:49 +0900

    add program option to sampleWords

--- ts/snippets/sampleWords.ts
+++ ts/snippets/sampleWords.ts
@@ -1,13 +1,64 @@
+import yargs from 'yargs';
 import fs from 'fs/promises';
 
-import { sample } from '../utils/random';
+import { range } from '../utils/generators';
+import { getRandomInt } from '../utils/random';
+
+interface Options {
+  inputFile: string;
+  words: number;
+}
+
+const getOptions = (): Options => {
+  const args = yargs
+    .command('*', 'Convert text file to Shift_JIS encoding')
+    .options({
+      'input-file': {
+        type: 'string',
+        alias: 'i',
+        demandOption: true,
+        default: '/usr/share/dict/words',
+        describe: 'Input file path',
+      },
+      words: {
+        type: 'number',
+        alias: 'n',
+        demandOption: true,
+        default: 4,
+        describe: 'word count to sample (all line if 0 is given)',
+      },
+    })
+    .alias({
+      h: 'help',
+    })
+    .version(false)
+    .strict()
+    .parseSync();
+
+  return {
+    inputFile: args['input-file'],
+    words: args['words'],
+  };
+};
 
 const main = async () => {
-  const contents = await fs.readFile('/usr/share/dict/words', { encoding: 'utf-8' });
-  const allWords = contents.split('\n');
-  const chosenWords = sample(4, allWords);
-  chosenWords.forEach((word) => {
-    console.log(word);
-  });
+  const { inputFile, words } = getOptions();
+  const contents = await fs.readFile(inputFile, { encoding: 'utf-8' });
+  const allWords = contents.split('\n').filter((str) => str !== '');
+
+  const len = allWords.length;
+  const n = words <= 0 ? len : words;
+  for (const i of range(n)) {
+    if (i >= len) {
+      break;
+    }
+    const j = getRandomInt(i, len);
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const sampled = allWords[j]!;
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    allWords[j] = allWords[i]!;
+    allWords[i] = sampled;
+    console.log(sampled);
+  }
 };
 void main();
