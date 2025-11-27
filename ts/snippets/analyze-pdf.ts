d803964af9016e686ab6c9c06d2045e33f17e895
Takaya SAITO <saito.takaya@plus-zero.co.jp>
2025-11-27 22:40:08 +0900

    use getOperatorList to extract PDF

--- ts/snippets/analyze-pdf.ts
+++ ts/snippets/analyze-pdf.ts
@@ -37,6 +37,28 @@ const getOptions = (): Options => {
   };
 };
 
+const operatorNumberToNameMap = new Map<number, string>(
+  Object.entries(pdfjsLib.OPS)
+    .map(([key, num]): [number, string] | null => {
+      if (typeof num !== 'number') {
+        return null;
+      }
+      return [num, key];
+    })
+    .filter((elem) => elem != null),
+);
+
+type UnwrapTupleArray<T extends readonly (readonly unknown[])[]> = {
+  [Index in keyof T]: T[Index][number];
+};
+
+function zip<Args extends readonly (readonly unknown[])[]>(...args: Args): UnwrapTupleArray<Args>[] {
+  return Array.from(
+    { length: Math.min(...args.map((arg) => arg.length)) },
+    (_, idx) => args.map((arg) => arg[idx]) as UnwrapTupleArray<Args>,
+  );
+}
+
 async function main() {
   const { inputFile, outputFile } = getOptions();
   const pdfData = new Uint8Array(await fs.readFile(inputFile));
@@ -46,7 +68,17 @@ async function main() {
   const pages = await Promise.all(
     Array.from({ length: maxPages }, (_, i) => i + 1).map(async (pageNumber) => {
       const page = await pdf.getPage(pageNumber);
-      return page.getTextContent();
+      const { fnArray, argsArray } = await page.getOperatorList();
+      return zip(
+        fnArray.map((fnNum) => {
+          const fn = operatorNumberToNameMap.get(fnNum);
+          if (fn == null) {
+            return String(fnNum);
+          }
+          return fn;
+        }),
+        argsArray,
+      );
     }),
   );
 
