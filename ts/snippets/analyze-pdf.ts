41f4bccc1d1c631dfa49ebbe0a1122a8590cb747
Takaya SAITO <saito.takaya@plus-zero.co.jp>
2025-12-03 19:35:10 +0900

    add text analyze mode

--- ts/snippets/analyze-pdf.ts
+++ ts/snippets/analyze-pdf.ts
@@ -2,9 +2,13 @@ import * as fs from 'fs/promises';
 import yargs from 'yargs';
 import * as pdfjsLib from 'pdfjs-dist/legacy/build/pdf.mjs';
 
+const modes = ['operator', 'text'] as const;
+type Mode = (typeof modes)[number];
+
 interface Options {
   inputFile: string;
   outputFile: string;
+  mode: Mode;
 }
 const getOptions = (): Options => {
   const args = yargs
@@ -23,6 +27,13 @@ const getOptions = (): Options => {
         default: '/dev/stdout',
         describe: 'Output file path',
       },
+      mode: {
+        choices: modes,
+        alias: 'm',
+        demandOption: true,
+        default: 'operator' as const,
+        describe: 'What to output',
+      },
     })
     .alias({
       h: 'help',
@@ -34,19 +45,9 @@ const getOptions = (): Options => {
   return {
     inputFile: args['input-file'],
     outputFile: args['output-file'],
+    mode: args['mode'],
   };
 };
-
-const operatorNumberToNameMap = new Map<number, string>(
-  Object.entries(pdfjsLib.OPS)
-    .map(([key, num]): [number, string] | null => {
-      if (typeof num !== 'number') {
-        return null;
-      }
-      return [num, key];
-    })
-    .filter((elem) => elem != null),
-);
 
 type UnwrapTupleArray<T extends readonly (readonly unknown[])[]> = {
   [Index in keyof T]: T[Index][number];
@@ -60,27 +61,56 @@ function zip<Args extends readonly (readonly unknown[])[]>(...args: Args): Unwra
 }
 
 async function main() {
-  const { inputFile, outputFile } = getOptions();
+  const { inputFile, outputFile, mode } = getOptions();
   const pdfData = new Uint8Array(await fs.readFile(inputFile));
   const pdf = await pdfjsLib.getDocument({ data: pdfData }).promise;
   const maxPages = pdf.numPages;
 
-  const pages = await Promise.all(
-    Array.from({ length: maxPages }, (_, i) => i + 1).map(async (pageNumber) => {
-      const page = await pdf.getPage(pageNumber);
-      const { fnArray, argsArray } = await page.getOperatorList();
-      return zip(
-        fnArray.map((fnNum) => {
-          const fn = operatorNumberToNameMap.get(fnNum);
-          if (fn == null) {
-            return String(fnNum);
-          }
-          return fn;
-        }),
-        argsArray,
-      );
-    }),
-  );
+  const pages = await (async () => {
+    switch (mode) {
+      case 'operator': {
+        const operatorNumberToNameMap = new Map<number, string>(
+          Object.entries(pdfjsLib.OPS)
+            .map(([key, num]): [number, string] | null => {
+              if (typeof num !== 'number') {
+                return null;
+              }
+              return [num, key];
+            })
+            .filter((elem) => elem != null),
+        );
+        return Promise.all(
+          Array.from({ length: maxPages }, (_, i) => i + 1).map(async (pageNumber) => {
+            const page = await pdf.getPage(pageNumber);
+
+            const { fnArray, argsArray } = await page.getOperatorList();
+            return zip(
+              fnArray.map((fnNum) => {
+                const fn = operatorNumberToNameMap.get(fnNum);
+                if (fn == null) {
+                  return String(fnNum);
+                }
+                return fn;
+              }),
+              argsArray,
+            );
+          }),
+        );
+      }
+      case 'text':
+        return Promise.all(
+          Array.from({ length: maxPages }, (_, i) => i + 1).map(async (pageNumber) => {
+            const page = await pdf.getPage(pageNumber);
+            return {
+              viewport: page.getViewport(),
+              textContent: await page.getTextContent(),
+            };
+          }),
+        );
+      default:
+        mode satisfies never;
+    }
+  })();
 
   await fs.writeFile(outputFile, JSON.stringify(pages, null, 2));
 }
