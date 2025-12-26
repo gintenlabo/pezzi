d48e68abf083042dc13b806be8a1fcebb9c7fe13
Takaya SAITO <saito.takaya@plus-zero.co.jp>
2025-12-26 21:07:38 +0900

    change th-dnh microthread design to use generator

--- ts/th-dnh/microthread.ts
+++ ts/th-dnh/microthread.ts
@@ -1,88 +1,52 @@
-class Queue<T> {
-  private stackPush: T[];
-  private stackPop: T[];
-
-  constructor() {
-    this.stackPush = [];
-    this.stackPop = [];
-  }
-
-  enqueue(value: T) {
-    this.stackPush.push(value);
-  }
-
-  dequeue(): T | undefined {
-    if (this.stackPop.length === 0) {
-      while (this.stackPush.length > 0) {
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        this.stackPop.push(this.stackPush.pop()!);
-      }
-    }
-    return this.stackPop.pop();
-  }
-
-  count(): number {
-    return this.stackPush.length + this.stackPop.length;
-  }
-}
+type GeneratorType = Generator<void, void, void>;
 
 export class MicrothreadManager {
-  private queue: Queue<() => void>;
+  private nextTasks: Array<GeneratorType>;
 
   constructor() {
-    this.queue = new Queue();
+    this.nextTasks = [];
   }
 
-  yield(): Promise<void> {
-    return new Promise<void>((resolve) => {
-      this.queue.enqueue(() => {
-        resolve();
-      });
-      setTimeout(() => {
-        const nextTask = this.queue.dequeue();
-        if (nextTask != null) {
-          nextTask();
+  yield(): boolean {
+    const tasks = this.nextTasks;
+    this.nextTasks = [];
+    tasks.forEach((task) => {
+      try {
+        const { done } = task.next();
+        if (!done) {
+          this.nextTasks.push(task);
         }
-      }, 0);
-      /*
-      const nextTask = this.queue.dequeue();
-      if (nextTask == null) {
-        resolve();
-        return;
+      } catch (e) {
+        console.error(e);
       }
-      this.queue.enqueue(() => {
-        resolve();
-      });
-      nextTask();
-       */
     });
+    return this.nextTasks.length > 0;
   }
 
-  startTask<Args extends unknown[]>(task: (...args: Args) => Promise<void>, ...args: Args): void {
-    task(...args)
-      .catch((err: unknown) => {
-        console.error(err);
-      })
-      .finally(() => {
-        const nextTask = this.queue.dequeue();
-        if (nextTask != null) {
-          nextTask();
-        }
-      });
+  startTask<Args extends unknown[]>(task: (...args: Args) => GeneratorType, ...args: Args): void {
+    const gen = task(...args);
+    const { done } = gen.next();
+    if (!done) {
+      this.nextTasks.push(gen);
+    }
   }
 
-  wrapTask<Args extends unknown[]>(task: (...args: Args) => Promise<void>): (...args: Args) => void {
+  wrapTask<Args extends unknown[]>(task: (...args: Args) => GeneratorType): (...args: Args) => void {
     return (...args) => {
       this.startTask(task, ...args);
     };
+  }
+
+  countTasks(): number {
+    return this.nextTasks.length;
   }
 }
 
 const globalManager = new MicrothreadManager();
 
-export const dnh_yield = (): Promise<void> => globalManager.yield();
-export const startTask = <Args extends unknown[]>(task: (...args: Args) => Promise<void>, ...args: Args): void => {
+export const yield_all = () => globalManager.yield();
+export const startTask = <Args extends unknown[]>(task: (...args: Args) => GeneratorType, ...args: Args): void => {
   globalManager.startTask(task, ...args);
 };
-export const wrapTask = <Args extends unknown[]>(task: (...args: Args) => Promise<void>): ((...args: Args) => void) =>
+export const wrapTask = <Args extends unknown[]>(task: (...args: Args) => GeneratorType): ((...args: Args) => void) =>
   globalManager.wrapTask(task);
--- ts/th-dnh/sample.ts
+++ ts/th-dnh/sample.ts
@@ -1,21 +1,25 @@
-import { dnh_yield, wrapTask } from './microthread';
+import { yield_all, wrapTask } from './microthread';
 
-const X = wrapTask(async (a: string) => {
+const X = wrapTask(function* (a: string) {
   console.log(`(1): X(${JSON.stringify(a)})`);
-  await dnh_yield();
+  yield;
   console.log(`(3): X(${JSON.stringify(a)})`);
   throw new Error('Error!');
 });
 
-async function main() {
-  await dnh_yield();
+const main = wrapTask(function* () {
+  yield;
   X('arg1');
   X('arg2');
   console.log('(2)');
-  await dnh_yield();
+  for (let i = 0; i < 10; i++) {
+    yield;
+  }
+  yield;
   console.log('(4)');
-}
-
-main().catch((err: unknown) => {
-  console.error(err);
+  yield;
 });
+
+main();
+
+while (yield_all());
